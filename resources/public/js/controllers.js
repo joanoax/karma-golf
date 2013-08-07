5692eda2943bd6de9fe786045b56e54237f0b9b4
John Eikens <jeikens@Joans-MacBook-Air.local>
2013-08-07 12:37:15 -0700

    Added a page for testing Zillow coding exercise.

--- resources/public/js/controllers.js
+++ resources/public/js/controllers.js
@@ -3,9 +3,20 @@
 /* Controllers */
 
 angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
-
+  controller('gridCtrl', [function($scope) {
+      
   }])
   .controller('MyCtrl2', [function() {
 
   }]);
+
+function gridCtrl($scope) {
+  $scope.phones = [
+    {"name": "Nexus S",
+     "snippet": "Fast just got faster with Nexus S."},
+    {"name": "Motorola XOOM™ with Wi-Fi",
+     "snippet": "The Next, Next Generation tablet."},
+    {"name": "MOTOROLA XOOM™",
+     "snippet": "The Next, Next Generation tablet."}
+  ];
+}
